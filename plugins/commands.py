996ec1315c45e052b2a79007bc54fdf95d84bc01
kkhanyaseen <91598415+kkhanyaseen@users.noreply.github.com>
2022-07-13 11:24:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,10 +41,11 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [
+        buttons = [[
             InlineKeyboardButton('🧔Owner', url='https://t.me/kkhanyaseen')
+            ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help')
-           ]
+           ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -82,10 +83,11 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons =[
-            InlineKeyboardButton('🧔Owner', url='https://t.me/kkhanyaseen)
+        buttons = [[
+            InlineKeyboardButton('🧔Owner', url='https://t.me/kkhanyaseen')
+            ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help')
-        ]
+           ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
