82a73c17c04015070a753a5a36d7d98272d65869
kkhanyaseen <91598415+kkhanyaseen@users.noreply.github.com>
2022-07-13 10:53:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,8 +404,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('Owner', url='https://t.me/kkhanyaseen')
-        ], [
+            InlineKeyboardButton('🧔Owner', url='https://t.me/kkhanyaseen')
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
            ]]
         reply_markup = InlineKeyboardMarkup(buttons)
